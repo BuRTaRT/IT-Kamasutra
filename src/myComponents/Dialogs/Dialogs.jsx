e919cf37ada010a57c2f261fdabc6c9b7602eca3
unknown <bburtart@gmail.com>
2022-09-05 21:22:01 +0300

    before props to dialogs from index.js

--- src/myComponents/Dialogs/Dialogs.jsx
+++ src/myComponents/Dialogs/Dialogs.jsx
@@ -2,23 +2,36 @@ import React from "react";
 import s from './Dialogs.module.css'
 import {NavLink} from "react-router-dom";
 
-let activeClassName = ({isActive}) => isActive ? s.active : undefined;
-
+function Dialog(props){
+    let path =`/dialogs/${props.id}`
+    return(
+        <div className={s.dialog}><NavLink activeClassName={s.active} to={path}>{props.name}</NavLink></div>
+    )
+}
+function Message(props){
+    return(
+        <div className={s.message}>{props.message}</div>
+    )
+}
+let userData=[
+    {name:'misha',id:1},
+    {name:'pasha',id:2},
+    {name:'sasha',id:3},
+    {name:'igor',id:4},
+    {name:'stew',id:5}
+]
+let dialogs =userData.map((item)=> <Dialog key={item.id} name={item.name} id={item.id}/>);
 function Dialogs() {
     return (
         <div className={s.dialogs}>
             <div className={s.dialog_items}>
-                <div className={s.dialog}><NavLink className={activeClassName} to='/dialogs/1'>Misha</NavLink></div>
-                <div className={s.dialog}><NavLink className={activeClassName} to='/dialogs/2'>Pasha</NavLink></div>
-                <div className={s.dialog}><NavLink className={activeClassName} to='/dialogs/3'>Sasha</NavLink></div>
-                <div className={s.dialog}><NavLink className={activeClassName} to='/dialogs/4'>Igor</NavLink></div>
-                <div className={s.dialog}><NavLink className={activeClassName} to='/dialogs/5'>Stew</NavLink></div>
+                {dialogs}
 
             </div>
             <div className={s.messages}>
-                <div className={s.message}>hi</div>
-                <div className={s.message}>How are you</div>
-                <div className={s.message}>Go play cs</div>
+                <Message message='Hello'/>
+                <Message message='Hello my friend'/>
+                <Message message='go play cs'/>
             </div>
 
         </div>
